1169c928047bf2012ed89be3f9d0e0ca16fcc80d
Tra my <tramy06spt@gmail.com>
2026-08-17 09:37:57 +0700

    capnhatmoi

---
+++ vd1_17_8_2026/src/main/java/com/mycompany/vd1_17_8_2026/Vd1_17_8_2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vd1_17_8_2026;
+
+/**
+ *
+ * @author DELL
+ */
+public class Vd1_17_8_2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tramy!");
+    }
+}
